5d431b52538e92c8006a4f67e55d04b109678427
flnh <flnh57@gmail.com>
2020-01-26 04:34:26 +0100

    ADD text effect on open menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,26 @@
 var hamburger = document.querySelector("#hamburger");
 var fermerMenu = document.querySelector("#fermerMenu");
+var menu = document.querySelector("#menu");
+var logo = document.querySelector("#logo")
+var ajouter = document.querySelector("[for=\"ajouter\"]");
+var categorie = document.querySelector("[for=\"categorie\"]");
+var theme = document.querySelector("[for=\"theme\"]");
+
+hamburger.addEventListener("click", function () {
+  menu.className = "menuOuvert";
+  fermerMenu.className = "menuOuvert";
+  logo.className = "menuOuvert";
+  ajouter.className = "menuOuvert";
+  categorie.className = "menuOuvert";
+  theme.className = "menuOuvert";
+})
 
 fermerMenu.addEventListener("click", function () {
-  console.log("click");
+  menu.className = "";
+  fermerMenu.className = "";
+  logo.className = "";
+  ajouter.className = "";
+  categorie.className = "";
+  theme.className = "";
   hamburger.checked = false;
 })
