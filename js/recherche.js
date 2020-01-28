44e9886539b1f0e2dd193136f652ebad1dcbaed9
flnh <flnh57@gmail.com>
2020-01-28 15:29:06 +0100

    Change comportment search bar

--- js/recherche.js
+++ js/recherche.js
@@ -2,6 +2,7 @@ var titreCategorie = document.querySelector("#titreCategorie");
 var btnRecherche = document.querySelector("#recherche");
 var btnRechercheAnnuler = document.querySelector("#btnRechercheAnnuler");
 var btnRechercheValider = document.querySelector("#btnRechercheValider");
+var barreRecherche = document.querySelector("#barreRecherche");
 
 btnRecherche.addEventListener("click", function (e) {
   if (e.target == btnRecherche) {
@@ -11,8 +12,10 @@ btnRecherche.addEventListener("click", function (e) {
 })
 
 btnRechercheAnnuler.addEventListener("click", function (e) {
-  if (e.target == btnRechercheAnnuler) {
+  if (e.target == btnRechercheAnnuler && barreRecherche.value == "") {
     titreCategorie.className = "";
     btnRecherche.className = "";
+  } else {
+    barreRecherche.value = "";
   }
 })
