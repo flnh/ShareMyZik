9882ad4ed17e2e788c3df1bddd878ca146c8eafb
flnh <flnh57@gmail.com>
2020-02-03 19:46:40 +0100

    Changing rendering method

--- js/musiques.js
+++ js/musiques.js
@@ -1,20 +1,19 @@
 var article = document.querySelector('article');
 
 ajax("./server/musiques.php", function (reponse) {
-  // ORDRE CROISSANT
-  // reponse.forEach(function (musique) {
-  //   var section = document.createElement('section');
-  //   var img = document.createElement('div');
-  //   img.className = "img";
-  //   img.style.backgroundImage = "url(" + musique[0] + ")";
-  //   img.style.backgroundSize = "101px 88px";
-  //   section.appendChild(img);
-  //   section.insertAdjacentHTML('beforeend', "<div><h3>" + musique[1] + "</h3><p>" + musique[2] + "</p><div><p>" + musique[3] + "</p><p>" + musique[4] + "</p></div></div>");
-  //   article.appendChild(section);
-  // })
+  /*ORDRE CROISSANT
+  reponse.forEach(function (musique) {
+    var section = document.createElement('section');
+    var img = document.createElement('div');
+    img.className = "img";
+    img.style.backgroundImage = "url(" + musique[0] + ")";
+    img.style.backgroundSize = "101px 88px";
+    section.appendChild(img);
+    section.insertAdjacentHTML('beforeend', "<div><h3>" + musique[1] + "</h3><p>" + musique[2] + "</p><div><p>" + musique[3] + "</p><p>" + musique[4] + "</p></div></div>");
+    article.appendChild(section);
+  })
 
-  //ORDRE DECROISSANT
-  for (var i = reponse.length - 1; i >= 0; i--) {
+    for (var i = reponse.length - 1; i >= 0; i--) {
     var section = document.createElement('section');
     var img = document.createElement('div');
     img.className = "img";
@@ -22,6 +21,76 @@ ajax("./server/musiques.php", function (reponse) {
     img.style.backgroundSize = "101px 88px";
     section.appendChild(img);
     section.insertAdjacentHTML('beforeend', "<div><h3>" + reponse[i][1] + "</h3><p>" + reponse[i][2] + "</p><div><p>" + reponse[i][3] + "</p><p>" + reponse[i][4] + "</p></div></div>");
+    article.appendChild(section);
+  }*/
+
+/*appendChild insertAdjacentHTML
+var more = document.createElement("span");
+more.insertAdjacentHTML('beforeend', 'More content');
+elem.appendChild(more);
+173,507
+±3.23%
+79% slower
+
+appendChild TextNode
+var more = document.createElement("span");
+more.appendChild(document.createTextNode("More content"));
+elem.appendChild(more);
+653,114
+±3.44%
+20% slower
+
+appendChild textContent (no html parser are used here)
+var more = document.createElement("span");
+more.textContent = "More content";
+elem.appendChild(more);
+815,905
+±3.35%
+fastest
+
+insertAdjacentHTML
+elem.insertAdjacentHTML('beforeend', '<span>More content</span>');
+161,670
+±6.80%
+81% slower
+
+appendChild innerHTML
+var more = document.createElement("span");
+more.innerHTML = "More content";
+elem.appendChild(more);
+172,499
+±6.32%
+79% slower
+
+innerHTML+=
+elem.innerHTML += '<span>More content</span>';
+1,009
+±2.38%
+100% slower*/
+
+for (var i = reponse.length - 1; i >= 0; i--) {
+    var section = document.createElement('section');
+    var img = document.createElement('div');
+    img.className = "img";
+    img.style.backgroundImage = "url(" + reponse[i][0] + ")";
+    img.style.backgroundSize = "101px 88px";
+    section.appendChild(img);
+    var divPrincipale = document.createElement('div');
+    var titre = document.createElement('h3');
+    titre.innerText = reponse[i][1];
+    divPrincipale.appendChild(titre);
+    var artiste = document.createElement('p');
+    artiste.innerText = reponse[i][2];
+    divPrincipale.appendChild(artiste);
+    var sousDivPrincipale = document.createElement('div');
+    var duree = document.createElement('p');
+    duree.innerText = reponse[i][3];
+    sousDivPrincipale.appendChild(duree);
+    var genre = document.createElement('p');
+    genre.innerText = reponse[i][4];
+    sousDivPrincipale.appendChild(genre);
+    divPrincipale.appendChild(sousDivPrincipale);
+    section.appendChild(divPrincipale);
     article.appendChild(section);
   }
 })
