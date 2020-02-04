6123a78650c3f566e22c6871dfe5bcfee1ce37ce
flnh <flnh57@gmail.com>
2020-02-04 10:18:36 +0100

    ADD bdd

--- server/musiques.php
+++ server/musiques.php
@@ -1,13 +1,36 @@
 <?php
-  $tabMusiques = [];
-  for ($i=0; $i < 1000; $i++) {
-    $url = "./index.html";
-    $image = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/No_sign.svg/1024px-No_sign.svg.png";
-    $titre = "Test " . $i;
-    $artiste = "Monsieur Test " . $i;
-    $duree = $i . "min";
-    $genre = "zebiDance";
-    $tabMusiques[] = [$url, $image, $titre, $artiste, $duree, $genre];
+  // $tabMusiques = [];
+  // for ($i=0; $i < 1000; $i++) {
+  //   $url = "./index.html";
+  //   $image = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/No_sign.svg/1024px-No_sign.svg.png";
+  //   $titre = "Test " . $i;
+  //   $artiste = "Monsieur Test " . $i;
+  //   $duree = $i . "min";
+  //   $genre = "zebiDance";
+  //   $tabMusiques[] = [$url, $image, $titre, $artiste, $duree, $genre];
+  // }
+  // echo json_encode($tabMusiques);
+  $bdd = new PDO('mysql:host=localhost;dbname=ShareMyZik;charset=utf8', 'root', '');
+
+  $req = $bdd->prepare('SELECT * FROM musiques ORDER BY ID DESC');
+  $req->execute();
+
+  $tabSortie = [];
+  $i = 0;
+
+  while($donnees = $req->fetch()) {
+    $tabSortie[$i]['ID'] = $donnees['ID'];
+    $tabSortie[$i]['Titre'] = $donnees['Titre'];
+    $tabSortie[$i]['Artiste'] = $donnees['Artiste'];
+    $tabSortie[$i]['Duree'] = $donnees['Duree'];
+    $tabSortie[$i]['Album'] = $donnees['Album'];
+    $tabSortie[$i]['Genre'] = $donnees['Genre'];
+    $tabSortie[$i]['UrlMusique'] = $donnees['UrlMusique'];
+    $tabSortie[$i]['UrlImage'] = $donnees['UrlImage'];
+    $i++;
   }
-  echo json_encode($tabMusiques);
+
+  $req->closeCursor();
+
+  echo json_encode($tabSortie);
 ?>
